2994027370efa769c8e8699fbc5113701b19259f
David Castillo <elrohirdrow1@gmail.com>
2015-11-16 12:43:08 +0100

    Primer Commit

---
+++ src/main/java/com/mongodb/App.java
@@ -0,0 +1,16 @@
+package com.mongodb;
+
+import spark.Spark;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+    }
+}
